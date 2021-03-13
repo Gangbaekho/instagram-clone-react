78eecc489ebbb285419a11e204793d3611216724
kimjinsoo <roorooro9933@gmail.com>
2021-03-13 22:09:58 +0900

    carousel library

--- src/page/TestPage.js
+++ src/page/TestPage.js
@@ -1,10 +1,22 @@
 import React from "react";
-import MainFooter from "../component/common/MainFooter";
+import Carousel from "react-elastic-carousel";
+
+const items = [
+  { id: 1, title: "item #1" },
+  { id: 2, title: "item #2" },
+  { id: 3, title: "item #3" },
+  { id: 4, title: "item #4" },
+  { id: 5, title: "item #5" },
+];
 
 const TestPage = (props) => {
   return (
     <div>
-      <MainFooter />
+      <Carousel>
+        {items.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </Carousel>
     </div>
   );
 };
