b03d93896f7107ab476dd367e05d5a24cfa5a985
kimjinsoo <roorooro9933@gmail.com>
2021-03-15 13:35:23 +0900

    simple feed form and request create feed

--- src/page/AddPage.js
+++ src/page/AddPage.js
@@ -1,10 +1,48 @@
 import React from "react";
 import MainHeader from "../component/common/MainHeader";
 
+import { fetchPostAPIWithJWT } from "../utils/fetchApis";
+
+import { useHistory } from "react-router-dom";
+
 const AddPage = (props) => {
+  const history = useHistory();
+
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    const content = e.target.elements.content.value;
+    const contentUrl = e.target.elements.contentUrls.value;
+
+    fetchPostAPIWithJWT("/feed/", {
+      body: { content: content, contentUrl: contentUrl },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <MainHeader />
+      <div className="border-red-400 border-2 border-solid mt-20 ">
+        <form onSubmit={submitFormHandler}>
+          <div>
+            <label htmlFor="content">Content:</label>
+            <textarea id="content" placeholder="text here"></textarea>
+          </div>
+          <div>
+            <label htmlFor="contentUrls">contentUrls:</label>
+            <input type="text" id="contentUrls" placeholder="text me" />
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
