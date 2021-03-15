a7f024b9b1648f4dcbf0a55a0d48d8e10484c44d
kimjinsoo <roorooro9933@gmail.com>
2021-03-15 18:45:44 +0900

    test create reply to server

--- src/page/TestPageTwo.js
+++ src/page/TestPageTwo.js
@@ -1,10 +1,37 @@
 import React from "react";
 
-import Carousel from "../component/common/StoryCarousel";
-import CarouselItem from "../component/common/StoryCarouselItem";
+import { fetchPostAPIWithJWT } from "../utils/fetchApis";
 
 const TestPageTwo = (props) => {
-  return <div>This is Test Page Two</div>;
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+    const content = e.target.elements.reply.value;
+
+    const bodyData = { content: content };
+
+    fetchPostAPIWithJWT("/reply/", { body: bodyData })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="border-2 border-black border-solid">
+      <form onSubmit={formSubmitHandler}>
+        <div>
+          <label htmlFor="reply">REPLY:</label>
+          <input type="text" placeholder="REPLY TEXT" id="reply" name="reply" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default TestPageTwo;
