f102873ee9624aebb37fbf8349faa7ce9bb1af39
kimjinsoo <roorooro9933@gmail.com>
2021-03-16 13:36:59 +0900

    rendering feeds with fetched feeds data from server

--- src/component/feed/Feed.js
+++ src/component/feed/Feed.js
@@ -1,26 +1,34 @@
 import React from "react";
 
-import defaultUser from "../../image/default_user.s.png";
+import SERVER_ADDRESS from "../../constant/serverAddress";
+
+// import defaultUser from "../../image/default_user.s.png";
 
 const Feed = (props) => {
   return (
     <section className="border-blue-500 border-solid border-2">
       <article className="flex justify-between items-center border-red-300 border-solid border-2 h-14">
         <div>
-          <img src={defaultUser} className="w-10 inline align-middle" />
-          <span className="align-middle">NICKNAME</span>
+          <img
+            src={SERVER_ADDRESS + "/" + props.userProfileImageUrl}
+            className="w-10 inline align-middle"
+          />
+          <span className="align-middle">{props.userNickName}</span>
         </div>
         <div>dotdotdot</div>
       </article>
       <article>
-        <img src={defaultUser} className="w-full" />
+        <img
+          src={SERVER_ADDRESS + "/" + props.contentUrls[0]}
+          className="w-full"
+        />
       </article>
       <article>
         <div className="flex justify-between items-center px-5">
           <div>ICONS</div>
           <div>SHARE ICON</div>
         </div>
-        <div className="px-5">LIKE COUNT</div>
+        <div className="px-5">LIKE COUNT: {props.likeCount}</div>
         <div className="px-5">REPLY AREA</div>
       </article>
       <article className="grid grid-cols-10 px-5">
--- src/component/feed/FeedContainer.js
+++ src/component/feed/FeedContainer.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Feed from "./Feed";
 
+import SERVER_ADDRESS from "../../constant/serverAddress";
+
 const FeedContainer = (props) => {
+  const [feeds, setFeeds] = useState([]);
+
+  useEffect(() => {
+    fetch(`${SERVER_ADDRESS}/feed/`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setFeeds(data.feeds);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="mt-10 border-black border-2 border-solid ">
-      <Feed />
-      <Feed />
-      <Feed />
+      {feeds.map((feed) => {
+        return <Feed key={feed._id} {...feed} />;
+      })}
     </div>
   );
 };
