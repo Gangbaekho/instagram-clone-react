4cfdeb0c59910eff1f39ae5d8b4575964e487006
kimjinsoo <roorooro9933@gmail.com>
2021-03-18 13:40:34 +0900

    reply like request

--- src/component/feed/Reply.js
+++ src/component/feed/Reply.js
@@ -1,6 +1,26 @@
 import React from "react";
 
+import default_heart from "../../image/default_heart.s.png";
+import { fetchPostAPIWithJWT } from "../../utils/fetchApis";
+
 const Reply = (props) => {
+  const likeButtonHandler = () => {
+    const bodyData = {
+      replyId: props._id,
+    };
+
+    fetchPostAPIWithJWT("/reply/like", { body: bodyData })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <section className="flex px-5 justify-between border-2 border-black border-solid w-full">
       <article className="flex border-solid border-2 border-blue-200">
@@ -8,7 +28,9 @@ const Reply = (props) => {
         <div className="pl-2">{props.content}</div>
       </article>
       <article className="border-solid border-2 border-blue-200">
-        HEART ICON
+        <button onClick={likeButtonHandler}>
+          <img src={default_heart} className="w-6" />
+        </button>
       </article>
     </section>
   );
