120983fb8c6429da23080895dd5cdc3c27aec942
kimjinsoo <roorooro9933@gmail.com>
2021-03-22 13:17:18 +0900

    feed detail small size layout test

--- src/page/TestPage.js
+++ src/page/TestPage.js
@@ -2,10 +2,26 @@ import React, { useState, useRef, useEffect } from "react";
 
 const TestPage = (props) => {
   return (
-    <div className="w-12 border-black border-2 border-solid truncate">
-      asdjlajsdklajlsdkjaklsdjlkasjdlkajslkdjaslkdjalskjdklasjl
-      {/* <br>asjdjasd</br>
-      <br>hello</br> */}
+    <div className="flex flex-col justify-center w-screen h-screen border-2 border-solid border-black bg-black relative">
+      <div className="absolute top-10 right-10 bg-white">XICON</div>
+      <div className="flex justify-center mr-10 ml-10 border-black border-2 border-solid bg-white">
+        <div className="border-black border-2 border-solid w-60">
+          <article className="flex justify-center border-black border-2 border-solid">
+            <div className="border-black border-2 border-solid">사진</div>
+            <div className="border-black border-2 border-solid">이름</div>
+            <div className="border-black border-2 border-solid">팔로잉</div>
+            <div className="border-black border-2 border-solid">...</div>
+          </article>
+          <article className="border-black border-2 border-solid">사진</article>
+          <article className="flex border-black border-2 border-solid">
+            <div className="border-black border-2 border-solid">아이콘즈</div>
+            <div className="border-black border-2 border-solid">공유</div>
+          </article>
+          <article className="border-black border-2 border-solid">
+            좋아요
+          </article>
+        </div>
+      </div>
     </div>
   );
 };
