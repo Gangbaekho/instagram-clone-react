1a7a65ace4b2e3aeabb253e9741fff27e14a70c4
kimjinsoo <roorooro9933@gmail.com>
2021-03-22 13:31:33 +0900

    feed detail page layout test

--- src/page/TestPage.js
+++ src/page/TestPage.js
@@ -1,28 +1,43 @@
 import React, { useState, useRef, useEffect } from "react";
 
+import default_user from "../image/default_user.s.png";
+import default_heart from "../image/default_heart.s.png";
+import default_detail from "../image/default_detail.s.png";
+import default_share from "../image/default_share.s.png";
+
 const TestPage = (props) => {
   return (
-    <div className="flex flex-col justify-center w-screen h-screen border-2 border-solid border-black bg-black relative">
+    <section className="flex flex-col justify-center w-screen h-screen border-2 border-solid border-black bg-black relative">
       <div className="absolute top-10 right-10 bg-white">XICON</div>
       <div className="flex justify-center mr-10 ml-10 border-black border-2 border-solid bg-white">
         <div className="border-black border-2 border-solid w-60">
           <article className="flex justify-center border-black border-2 border-solid">
-            <div className="border-black border-2 border-solid">사진</div>
-            <div className="border-black border-2 border-solid">이름</div>
+            <div className="border-black border-2 border-solid w-10">
+              <img src={default_user} />
+            </div>
+            <div className="border-black border-2 border-solid">Ronaldo</div>
             <div className="border-black border-2 border-solid">팔로잉</div>
             <div className="border-black border-2 border-solid">...</div>
           </article>
-          <article className="border-black border-2 border-solid">사진</article>
-          <article className="flex border-black border-2 border-solid">
-            <div className="border-black border-2 border-solid">아이콘즈</div>
-            <div className="border-black border-2 border-solid">공유</div>
+          <article className="border-black border-2 border-solid">
+            <img src={default_user} className="w-full" />
+          </article>
+          <article className="flex justify-between border-black border-2 border-solid">
+            <div className="border-black border-2 border-solid">
+              <img src={default_heart} className="w-10 inline-block" />
+              <img src={default_detail} className="w-10 inline-block" />
+            </div>
+            <div className="border-black border-2 border-solid">
+              <img src={default_share} className="w-10 inline-block" />
+            </div>
           </article>
           <article className="border-black border-2 border-solid">
-            좋아요
+            좋아요 100개
           </article>
+          <article>2일전</article>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
